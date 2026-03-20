3316d50bd30370ace0af8aa0fb58228c5501d94d
Rahin Haque <haquerahin743@gmail.com>
2026-03-20 09:36:34 +0600

    Displayed cards dynamically

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,14 +1,21 @@
 import React, { use } from 'react'
+import Country from '../Country/Country';
+import './countries.css';
 
 const Countries = ({ CountriesPromise }) => {
   const countriesData = use(CountriesPromise);
   const countries = countriesData.countries;
   console.log(countries);
-  return <div>
-   
-    All Countries : {countries.length}
-
-  </div>;
+  return (
+    <div >
+      All Countries : {countries.length}
+      <div className="countries">
+        {countries.map((country) => (
+          <Country country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './country.css'
 
-const Country = () => {
+const Country = ({country}) => {
+ console.log(country.flags.flags);
  return (
-  <div>
-   
-  </div>
+   <div className="country">
+     <img src={country.flags.flags.png} alt="" />
+     <h3>Name :{country.name.common}</h3>
+     <p>Population : {country.population.population}</p>
+   </div>
  );
 };
 
